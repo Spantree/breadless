6844fdc19de6263bd324a19f90e7aa1ab85a71b4
Kevin Greene <kevin@spantree.net>
2014-02-08 12:16:16 -0600

    Added basic ingredient controller / spec

---
+++ breadless-web/test/unit/net/spantree/breadless/IngredientControllerSpec.groovy
@@ -0,0 +1,11 @@
+package net.spantree.breadless
+
+import grails.test.mixin.Mock
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+@TestFor(IngredientController)
+@Mock([Ingredient])
+class IngredientControllerSpec extends Specification {
+
+}
