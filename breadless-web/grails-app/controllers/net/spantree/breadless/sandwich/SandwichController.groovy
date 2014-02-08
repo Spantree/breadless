118941377b948ee730de04d88bcbe98653f38fea
Gary Turovsky <flyhighplato@gmail.com>
2014-02-08 13:50:14 -0600

    Moving sandwich controller into appropriate folder

---
+++ breadless-web/grails-app/controllers/net/spantree/breadless/sandwich/SandwichController.groovy
@@ -0,0 +1,52 @@
+package net.spantree.breadless.sandwich
+
+import grails.converters.JSON
+import net.spantree.breadless.Competition
+import net.spantree.breadless.Sandwich
+
+class SandwichController {
+    def ingredientService
+
+    def show = {
+
+        withFormat {
+            html {
+               render view:""
+            }
+
+            json {
+                def id = params.id
+                def sandwich = Sandwich.findById(id)
+                def json = [:]
+                if (!sandwich) {
+                    json = [msg: 'Sandwich not found']
+                } else {
+                    json << [creator: sandwich.creator]
+                    json << [competition: sandwich.competition.id]
+                    json << [ingredients: ingredientService.formatIngredients(sandwich.ingredients)]
+                }
+                render json as JSON
+            }
+        }
+    }
+
+    def submit = {
+        def competition = Competition.findByName("Original") // Competition.findById(params.id)
+        def name = params.name
+        def creator = getAuthenticationUser()
+        def ingredients = params.ingredients.collect {
+            Ingredient.findByName(it)
+        }
+
+        def sandwich = new Sandwich(
+                name: name,
+                competition: competition,
+                creator: creator,
+                ingredients: ingredients
+        )
+
+        sandwich.save()
+        def json = [message: "Saved sandwich with id ${sandwich.id}"]
+        render json as JSON
+    }
+}
