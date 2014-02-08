9ee9268d533eb51d5addbc54feca54b9cb315471
Kevin Greene <kevin@spantree.net>
2014-02-08 10:16:59 -0600

    U;dated build config

--- breadless-web/grails-app/conf/BuildConfig.groovy
+++ breadless-web/grails-app/conf/BuildConfig.groovy
@@ -31,17 +31,12 @@ grails.project.dependency.resolution = {
         mavenLocal()
         mavenCentral()
 
-        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
     }
 
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
+        test 'org.spockframework:spock-grails-support:0.7-groovy-2.0'
+        compile "joda-time:joda-time:2.1"
 
-        // runtime 'mysql:mysql-connector-java:5.1.22'
     }
 
     plugins {
@@ -56,15 +51,17 @@ grails.project.dependency.resolution = {
         compile ":twitter-bootstrap:2.3.0"
         compile ":underscore:1.4.4"
 
-        // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0"
-        //runtime ":cached-resources:1.0"
-        //runtime ":yui-minify-resources:0.1.5"
-
         build ":tomcat:$grailsVersion"
 
         runtime ":database-migration:1.3.2"
 
         compile ':cache:1.0.1'
+        test(':spock:0.7') {
+            excludes 'spock-grails-support', 'hibernate', 'grails-hibernate'
+        }
+        compile ":mongo-jodatime:0.1.4"
+
+        compile ':spring-security-core:1.2.7.3'
+
     }
 }
