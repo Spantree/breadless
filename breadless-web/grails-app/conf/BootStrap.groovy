c9585d8366444cffb55a777234acb31db0e7bd19
Malynda Chizek Frouard <malynda@spantree.net>
2014-02-08 12:00:55 -0600

    Initial commit of domain objects

--- breadless-web/grails-app/conf/BootStrap.groovy
+++ breadless-web/grails-app/conf/BootStrap.groovy
@@ -1,3 +1,6 @@
+import net.spantree.breadless.Competition
+import net.spantree.breadless.Ingredient
+import net.spantree.breadless.IngredientType
 import net.spantree.breadless.Role
 import net.spantree.breadless.User
 import net.spantree.breadless.UserRole
@@ -26,6 +29,20 @@ class BootStrap {
         if (!user.authorities.contains(userRole)) {
             UserRole.create(user, userRole)
         }
+
+        def turkey = Ingredient.findByName("Turkey") ?: new Ingredient(name: "Turkey", type: IngredientType.MEAT).save()
+        def ham = Ingredient.findByName("Turkey") ?:new Ingredient(name: "Ham", type: IngredientType.MEAT).save()
+        def bologna = Ingredient.findByName("Turkey") ?:new Ingredient(name: "Bologna", type: IngredientType.MEAT).save()
+        def americanCheese = Ingredient.findByName("Turkey") ?:new Ingredient(name: "American Cheese", type: IngredientType.MEAT).save()
+        def pickles = Ingredient.findByName("Turkey") ?:new Ingredient(name: "Pickles", type: IngredientType.MEAT).save()
+        def lettuce = Ingredient.findByName("Turkey") ?:new Ingredient(name: "Lettuce", type: IngredientType.MEAT).save()
+        def tomatoes =Ingredient.findByName("Turkey") ?: new Ingredient(name: "Tomatoes", type: IngredientType.MEAT).save()
+        def onions = Ingredient.findByName("Turkey") ?:new Ingredient(name: "Onions", type: IngredientType.MEAT).save()
+        def provolone = Ingredient.findByName("Turkey") ?:new Ingredient(name: "Provolone", type: IngredientType.MEAT).save()
+        def mayonnaise = Ingredient.findByName("Turkey") ?:new Ingredient(name: "Mayonnaise", type: IngredientType.MEAT).save()
+        def mustard = Ingredient.findByName("Turkey") ?:new Ingredient(name: "Mustard", type: IngredientType.MEAT).save()
+
+        def originalComp = new Competition(name: "Original").save()
 
     }
     def destroy = {
---
+++ breadless-web/test/unit/breadless/web/Sandwich_competitionTests.groovy
@@ -0,0 +1,17 @@
+package breadless.web
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Sandwich_competition)
+class Sandwich_competitionTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
